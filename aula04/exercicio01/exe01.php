c1f53c810a708435a98bf920498e89618538fe93
Murilo <161628769+Murilo1006@users.noreply.github.com>
2025-03-07 17:01:46 -0300

    Add files via upload

---
+++ aula04/exercicio01/exe01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_GET['cxnome'];
+    $N1 = $_GET['N1'];
+    $N2 = $_GET['N2'];
+    $N3 = $_GET['N3'];
+    $média = ($N1 + $N2 + $N3)/3;
+?>
---
+++ aula04/exercicio01/form01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulários</title>
+</head>
+<body>
+    <h1>CADRASTO</h1>
+    <form action="exe01.php" method="GET">
+        <p>Digite seu nome: <input type="text" name="cxnome" id=""required></p>
+        <p>Digite N1: <input type="number" name="N1" id="" min="1" max="10"></p>
+        <p>Digite N2: <input type="number" name="N2" id="" min="1" max="10"></p>
+        <p>Digite N3: <input type="number" name="N3" id="" min="1" max="10"></p>
+    <input type="submit" value="ENVIAR"></form>
+</body>
+</html>
